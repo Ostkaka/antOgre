0ecaec48587596480cd923e331de70c6b3cd5f1f
Ostkaka <tomasberglund005@gmail.com>
2014-04-23 23:48:35 +0200

    made antOgre project

---
+++ include/antOgre/IOgreApp.hpp
@@ -0,0 +1,213 @@
+#ifndef IOGREAPP_HPP_
+	#define IOGREAPP_HPP_
+
+#include <fstream>
+#include <stddef.h>
+#include <string.h>
+#include <vector>
+#include <ant/core_types.hpp>
+#include <ant_config.hpp>
+#include <ant\BaseGameLogic.hpp>
+
+namespace ant
+{
+	struct GameSettings
+	{
+		GameSettings(){ m_level = ""; }
+		~GameSettings(){}
+
+		std::string m_level;
+	};
+
+	/**
+	* Base app class for ogre based application
+	*/
+	class IOgreApp
+	{
+	public:
+		/**
+		* IApp deconstructor
+		*/
+		virtual ~IOgreApp();
+
+		/**
+		* Will return the most recent created IApp based class. Used for retrieving
+		* assets among other things
+		*/
+		static  IOgreApp* getApp();
+
+		/*
+		* Processes the command line for arguments
+		*/
+		virtual void processArguments(int argc, char* argv[]);
+
+		/**
+		* Inits the app instance
+		*/
+		bool init();
+
+		/**
+		* Valued of the exit-code given by the application by using quit.
+		*/
+		int run();
+
+		/**
+		* Used to check in case the application is still running
+		*/
+		bool isRunning() const;
+
+		/**
+		* Returns the current game loop update rate being
+		* used in Hz (updates per second)
+		*/
+		ant::Real getUpdateRate() const;
+
+		/**
+		* SetUpdateRate will set the game loop update to rate
+		*/
+		void setUpdateRate(float rate);
+
+		/**
+		* Quit will signal the Application to stop running.
+		*/
+		void quit(int exitCode = 1);
+
+		/**
+		* Get the game logic from the APP
+		*/
+		IGameLogic * getGameLogic();
+
+	protected:
+
+		/**
+		* Basic constructor
+		*/
+		IOgreApp(const std::string theTitle = "Ant Application");
+
+		/**
+		* Responsible for monitoring IsRunning and exiting when the
+		* Application is done.
+		*/
+		virtual void gameLoop();
+
+		/**
+		* Registers events for the game
+		*/
+		virtual void registerGameEvents(void){}
+
+		/**
+		* ProcessInput is responsible for performing all input processing for
+		* the game loop.
+		*/
+		virtual void processInput();
+
+		/**
+		* Initializes the game logic and views.
+		*/
+		virtual BaseGameLogic* initGameLogicAndView() = 0;
+
+	private:
+
+		/**
+		* Initializes the resource global resource cache
+		*/
+		bool initResourceCache();
+
+		/**
+		* Initializes the script engine
+		*/
+		bool initScriptEngine();
+
+		/**
+		* Initializes the event system
+		*/
+		bool initEventSystem();
+
+		/**
+		* Registers engine specific events that consists of the backbone
+		*/
+		void registerEngineEvents(void);
+
+		/**
+		* Initializes the Rendering window that
+		* will be used to display the games graphics.
+		*/
+		void initRenderer();
+
+		/**
+		* Renders the frame for the game, given that human views are connected to it
+		*/
+		void renderFrame(ant::DeltaTime fTime, ant::DeltaTime dt);
+
+		/**
+		* Responsible for registering and loading the
+		* application configuration file (settings.cfg)
+		*/
+		void initConfig();
+
+		/**
+		* Cleanup is responsible for performing any last minute Application
+		* cleanup steps before exiting the Application.
+		*/
+		void cleanup();
+
+		/**
+		* App copy constructor is private because we do not allow copies of
+		* singleton class. Intentionally undefined.
+		*/
+		IOgreApp(const IOgreApp&);
+
+		/**
+		* Assignment operator is private because copies
+		* of our Singleton class are not allowed. Intentionally undefined.
+		*/
+		IOgreApp& operator=(const IOgreApp&);
+
+	public:
+		///CONSTANTS
+		/// Default Video width
+		static const unsigned int DEFAULT_VIDEO_WIDTH = 800;
+		/// Default Video height 
+		static const unsigned int DEFAULT_VIDEO_HEIGHT = 600;
+		/// Default video bits per pixel (color depth) 
+		static const unsigned int DEFAULT_VIDEO_BPP = 32;
+		/// Default application wide settings file string
+		static const char* APP_SETTINGS;
+
+		//////////////////////////////////////////////////////////////////////////
+		// Variables
+		//////////////////////////////////////////////////////////////////////////
+		/// Title to use for Window
+		std::string                  m_title;
+
+		/// Settings for the application	
+		GameSettings                 m_settings;
+
+		/// The game logic
+		BaseGameLogic*			     m_gameLogic;
+
+		// EventManager
+		EventManager*				 m_eventManager;
+
+	private:
+		/// Static instance variables assigned at creation
+		static IOgreApp*        g_App;
+
+		/// Exit code that is returned upon existing the application
+		int						m_exitCode;
+
+		/// TRUE if the app is still running
+		bool					m_running;
+
+		/// True if the application is initialized
+		bool					m_initialized;
+
+		/// Value that holds the update rate in seconds used for a fixed loop time
+		ant::DeltaTime m_updateRate;
+	};
+}
+
+
+
+
+#endif /*IOGREAPP_HPP_*/
---
+++ src/antOgre/IOgreApp.cpp
@@ -0,0 +1,277 @@
+#include <antOgre/IOgreApp.hpp>
+#include <ant/BaseGameLogic.hpp>
+#include <ant/IEventManager.hpp>
+#include <ant/Events.hpp>
+#include <ant/ResourceCacheManager.hpp>
+#include <ant/ResourceCache.hpp>
+#include <ant/ResourceFiles.hpp>
+#include <ant/TextureResource.hpp>
+#include <ant/Resource.hpp>
+#include <antLua/LuaStateManager.hpp>
+#include <antLua/ScriptProcess.hpp>
+#include <antLua/LuaScriptExports.hpp>
+#include <antLua/BaseScriptComponent.hpp>
+#include <ant/GenericObjectFactory.hpp>
+#include <OGRE/OgreTimer.h>
+
+using namespace ant;
+
+const char* SCRIPT_PREINIT_FILE = "lua\\PreInit.lua";
+
+/// Single instance of the most recently created IOgreApp class
+IOgreApp* IOgreApp::g_App = NULL;
+
+ant::IOgreApp::IOgreApp(const std::string theTitle /*= "Ant Application"*/)
+: m_gameLogic(NULL),
+m_eventManager(NULL),
+m_running(false),
+m_updateRate(0.016),
+m_initialized(false),
+m_title(theTitle)
+{
+	g_App = this;
+}
+
+ant::IOgreApp::~IOgreApp()
+{
+
+}
+
+void ant::IOgreApp::processArguments(int argc, char* argv[])
+{
+	// TODO
+}
+
+bool ant::IOgreApp::initResourceCache()
+{
+	bool resCacheInitialized = false;
+
+	// Init the resource cache in the resource handler singleton
+	std::string zipPath = ANT_DATA_PATH"\\unittestdata\\zipdata.zip";
+	IResourceFile * resFile = ANT_NEW DevelopmentResourceZipFile(L"Assets.zip", DevelopmentResourceZipFile::Editor);
+
+	ResourceCacheManager::startUp();
+	resCacheInitialized = ResourceCacheManager::instance()->initResourceCache(50, resFile);
+
+	// Declare and find the resource loaders
+	extern IResourceLoaderStrongPtr CreateXmlResourceLoader();
+	extern IResourceLoaderStrongPtr CreateScriptResourceLoader();
+	extern IResourceLoaderStrongPtr CreatePNGResourceLoader();
+	extern IResourceLoaderStrongPtr CreateJPGResourceLoader();
+
+	// Register the loaders 
+	ResourceCacheManager::instance()->getResourceCache()->registerLoader(CreateXmlResourceLoader());
+	ResourceCacheManager::instance()->getResourceCache()->registerLoader(CreateScriptResourceLoader());
+	ResourceCacheManager::instance()->getResourceCache()->registerLoader(CreatePNGResourceLoader());
+	ResourceCacheManager::instance()->getResourceCache()->registerLoader(CreateJPGResourceLoader());
+
+	return resCacheInitialized;
+}
+
+bool ant::IOgreApp::init()
+{
+	// Init Logger
+	Logger::Init(ANT_DATA_PATH"\\default_logging.xml");
+
+	ANT_LOG("OgreApplication", "Init application");
+
+	// Init resource cache
+	if (!initResourceCache())
+	{
+		ANT_ERROR("Failed to initialize resource cache");
+	}
+
+	// init Lua state manager
+	if (!initScriptEngine())
+	{
+		ANT_ERROR("Failed to initialize LuaStateManager");
+	}
+
+	// init the event system
+	if (!initEventSystem())
+	{
+		ANT_ERROR("Failed to create EventManager.");
+	}
+
+	// Register events
+	registerEngineEvents();
+	registerGameEvents();
+
+	// Init renderer and application window
+	initRenderer();
+
+	// Init the game logic and the views from a virtual function create by child class
+	m_gameLogic = initGameLogicAndView();
+	if (!m_gameLogic)
+	{
+		return false;
+	}
+
+	m_initialized = true;
+
+	return true;
+}
+
+int ant::IOgreApp::run()
+{
+	ANT_LOG("OgreApplication", "Running application");
+	ANT_ASSERT(m_initialized);
+
+	// Init the application
+	m_running = true;
+
+	// Start the main loop of the application
+	gameLoop();
+
+	// Clean up all the stuff in the application
+	cleanup();
+
+	m_running = false;
+
+	return 1;
+}
+
+void ant::IOgreApp::gameLoop()
+{
+	ANT_LOG("OgreApplication", "Starting gameloop");
+
+	// Init the update clock
+	Ogre::Timer updateClock;
+
+	// Init the frame clock
+	Ogre::Timer frameClock;
+
+	// Start up the clock
+	updateClock.reset();
+
+	// When do we update next?
+	int updateNext = updateClock.getMilliseconds();
+
+	// Begin the main loop
+	while (isRunning())
+	{
+		// Get the current time
+		ant::DeltaTime updateTime = ant::Real(updateClock.getMilliseconds() * 1000.0);
+
+		// Is it time to update yet?
+		ant::DeltaTime dt = ant::Real(frameClock.getMilliseconds() * 1000.0);
+		if (dt > m_updateRate)
+		{
+			// Process input and get it to the game logic and it's views
+			processInput();
+
+			// Process events
+			IEventManager::instance()->update(); // allow event queue to process for up to 20 ms
+
+			// Update game logic
+			m_gameLogic->onUpdate(updateTime, dt);
+
+			// Update renderer
+			renderFrame(updateTime, dt);
+
+			// Restart frame clock
+			frameClock.reset();
+		}
+	}
+}
+
+bool ant::IOgreApp::isRunning() const
+{
+	return m_running;
+}
+
+ant::Real ant::IOgreApp::getUpdateRate() const
+{
+	return m_updateRate;
+}
+
+void ant::IOgreApp::setUpdateRate(float rate)
+{
+	m_updateRate = rate;
+}
+
+void ant::IOgreApp::quit(int exitCode /*= 1*/)
+{
+	m_exitCode = exitCode;
+	m_running = false;
+}
+
+void ant::IOgreApp::initRenderer()
+{
+	ANT_LOG("IOgreApp", "Init Renderer");
+
+}
+
+void ant::IOgreApp::processInput()
+{
+	
+}
+
+void ant::IOgreApp::renderFrame(ant::DeltaTime fTime, ant::DeltaTime dt)
+{
+	if (m_gameLogic)
+	{
+		for (GameViewList::iterator it = m_gameLogic->getGameViews()->begin(),
+			end = m_gameLogic->getGameViews()->end(); it != end; ++it)
+		{
+			(*it)->onRender(fTime, dt);
+		}
+	}
+
+	// TODO - g_pApp->m_pGame->VRenderDiagnostics();
+}
+
+bool ant::IOgreApp::initScriptEngine()
+{
+	bool luaInitialized = false;
+
+	// First, create the Lua state manager
+	luaInitialized = LuaStateManager::startUp();
+
+	// Load the pre-init file
+	{
+		Resource resource(SCRIPT_PREINIT_FILE);
+		ResourceHandleStrongPtr pPreInitResource = ResourceCacheManager::instance()->getResourceCache()->getResourceHandle(&resource);
+	}
+
+	// Register all functions exported from c++
+	LuaScriptExports::registerScripts();
+	IScriptProcess::registerScriptClass();
+	BaseScriptComponent::registerScriptFunctions();
+
+	return luaInitialized;
+}
+
+void ant::IOgreApp::cleanup()
+{
+	// Clean up the following
+
+	// Resource manager
+	ResourceCacheManager::shutDown();
+
+	// Lua
+	LuaScriptExports::unregisterScripts();
+	BaseScriptComponent::unregisterScriptFunctions();
+}
+
+bool ant::IOgreApp::initEventSystem()
+{
+	m_eventManager = ANT_NEW EventManager("SFMLApp Event Mgr", true);
+	return m_eventManager != NULL;
+}
+
+void ant::IOgreApp::registerEngineEvents(void)
+{
+	REGISTER_EVENT(EvtData_New_Actor);
+	REGISTER_EVENT(EvtData_Destroy_Actor);
+}
+
+IOgreApp * ant::IOgreApp::getApp()
+{
+	return g_App;
+}
+
+IGameLogic * ant::IOgreApp::getGameLogic()
+{
+	return m_gameLogic;
+}
